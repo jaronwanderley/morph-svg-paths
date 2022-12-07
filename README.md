77c7bfba9a61f6b2d15c52193b28437c22011189
Jaron Wanderley <jaronwanderley@gmail.com>
2022-12-07 13:09:27 -0300

    setup project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # morp-svg-paths
- A Tiny ~1.5Kb function to morph between paths with same commands.
+ A Tiny ~1.4Kb function to morph between paths with same commands.
---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vitest/config'
+import dts from 'vite-plugin-dts'
+
+// https://vitejs.dev/guide/build.html#library-mode
+export default defineConfig({
+  build: {
+    target: 'esnext',
+    minify: 'terser',
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'morphSVGPaths',
+      fileName: 'morph-svg-paths',
+      formats: ['es', 'umd', 'iife'],
+    },
+    rollupOptions: {
+      plugins: [
+        {
+          name: 'remove-collection-handlers',
+          transform(code, id) {
+            if (id.endsWith('reactivity.esm-bundler.js')) {
+              return code
+                .replace('mutableCollectionHandlers,', 'null,')
+                .replace('readonlyCollectionHandlers,', 'null,')
+            }
+          },
+        },
+      ],
+    },
+  },
+  test: {
+    coverage: {
+      reporter: ['text', 'html', 'json-summary'],
+      reportsDirectory: './docs/test',
+    },
+  },
+  plugins: [dts()],
+})
