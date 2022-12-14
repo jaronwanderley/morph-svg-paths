e64be627e89c3e438180c863b077092c1c4d6bc6
Jaron Wanderley <jaronwanderley@gmail.com>
2022-12-14 18:07:19 -0300

    update on docs

--- docs/js/directives.js
+++ docs/js/directives.js
@@ -1,17 +1,58 @@
-import { getSelector } from './utils.js'
+import { createEl, setClass, setStyle, typeOf } from './utils.js'
+
+export function rippleDirective(context) {
+  const { el, exp, get, effect } = context
+
+  const set = () => {
+    const options = exp ? get() : {}
+    if (typeOf(options) !== 'Object') {
+      console.error('The param need to be a Object!')
+      return
+    }
+    setStyle(el, {
+      position: 'relative',
+      overflow: 'hidden',
+    })
+    if (el.rippleClick)
+      el.removeEventListener('click', el.rippleClick)
+    el.rippleClick = (event) => {
+      // setup
+      const { pageX, pageY } = event
+      const { x, y, width, height } = el.getBoundingClientRect()
+      const buttonSize = width > height ? width : height
+      const buttonSizeString = `${buttonSize}px`
+      // remove any previous ripples
+      const ripples = [...el.querySelectorAll('.ripple')]
+      ripples.forEach(ripple => el.removeChild(ripple))
+      // create a new ripple
+      const ripple = createEl('span')
+      setClass(ripple, 'ripple')
+      el.appendChild(ripple)
+      // set the ripple to the click position and start animation
+      setStyle(ripple, {
+        width: buttonSizeString,
+        height: buttonSizeString,
+        top: `${pageY - y - buttonSize / 2}px`,
+        left: `${pageX - x - buttonSize / 2}px`,
+      })
+      setClass(ripple, 'ripple-effect')
+    }
+    el.addEventListener('click', el.rippleClick, false)
+  }
+
+  set()
+  effect(() => {
+    set()
+  })
+}
 
 export function windowSizeDirective(context) {
-  const { el, exp, get, effect } = context
+  const { exp, get, effect } = context
   window.resizeCallbacks = {
     ...window?.resizeCallbacks || {},
   }
-  window.onresize = onResize
 
-  function update() {
-    const callBack = exp ? get() : () => {}
-    window.resizeCallbacks[getSelector(el)] = callBack
-  }
-  function onResize() {
+  const set = () => {
     const {
       innerWidth: width,
       innerHeight: height,
@@ -30,9 +71,18 @@ export function windowSizeDirective(context) {
         })
       })
   }
+  const update = () => {
+    const callBack = exp && get()
+    if (typeOf(callBack) !== 'Function') {
+      console.error('The param need to be a Function!')
+      return
+    }
+    window.resizeCallbacks[exp] = callBack
+  }
 
+  window.onresize = set
   update()
-  onResize()
+  set()
 
   effect(() => {
     update()
--- docs/js/utils.js
+++ docs/js/utils.js
@@ -1,15 +1,63 @@
+// DOM related
+export const typeOf = element => typeof element == 'object' && element != null
+  ? element.constructor.name
+  : {}.toString.call(element).match(/\s(\w+)/)[1]
+
+export const createEl = type => document.createElement(type)
+
+export const setClass = (element, className) => className
+  .split(' ')
+  .map(name => element.classList.add(name))
+
+export const setStyle = (element, object) => Object
+  .entries(object)
+  .map(([key, value]) => element.style[key] = value)
+
+// Object related
+export const get = (path, obj = window) => path
+  .split(/[\.\[\]]+/).filter(e => e !== '')
+  .reduce((prev, curr) => prev ? prev[parseInt(curr) ? parseInt(curr) : curr] : null, obj || self)
+
+export const set = (path, object, value) => {
+  const parts = path.split(/[\]\[.]/).filter(string => !!string)
+  return parts.reduce((previous, current, index, array) => {
+    if (index + 1 === parts.length)
+      return previous[current] = value
+    return previous[current] = previous[current] ? previous[current] : !isNaN(parseFloat(array[index + 1])) ? [] : {}
+  }, object)
+}
+
+export const getListOfPaths = (obj = {}) => {
+  const addDelimiter = (a, b) => a ? `${a}.${b}` : b
+  const paths = (obj = {}, head = '') => {
+    return Object.entries(obj)
+      .reduce((result, [key, value]) => {
+        const fullPath = addDelimiter(head, key)
+        return typeOf(value) === 'Object'
+          ? result.concat(paths(value, fullPath))
+          : result.concat(fullPath)
+      }, [])
+  }
+  return paths(obj)
+}
+
+// Array related
+export const range = (start = 0, end = 0, step = 1) => Array(Math.floor((end - start) / step + 1))
+  .fill()
+  .map((_, index) => step * index + start)
+
 // Math related
 export const clamp = (value, min, max) => Math.min(Math.max(value, min), max)
 
 // Geometry related
-export function getDistance(p1, p2) {
+export const getDistance = (p1, p2) => {
   const x = p2.x - p1.x
   const y = p2.y - p1.y
   return Math.sqrt(x * x + y * y)
 }
 
 // Content related
-export async function loadText(path) {
+export const loadText = async (path) => {
   try {
     const result = await fetch(path)
     return await result.text()
@@ -18,7 +66,8 @@ export async function loadText(path) {
     return ''
   }
 }
-export async function loadJson(path) {
+
+export const loadJson = async (path) => {
   try {
     const result = await fetch(path)
     return await result.json()
@@ -29,7 +78,7 @@ export async function loadJson(path) {
 }
 
 // Browser related
-export function getSelector(elem) {
+export const getSelector = (elem) => {
   const {
     tagName,
     id,
@@ -49,7 +98,7 @@ export function getSelector(elem) {
   return `${getSelector(parentNode)}>${tagName}:nth-child(${childIndex})`
 }
 
-export function platform() {
+export const platform = () => {
   const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
   const isDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
   return {
@@ -59,4 +108,3 @@ export function platform() {
     isLight: !isDark,
   }
 }
-
